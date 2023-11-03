81bc8fa06f7f9296971e44d9531afbd9328100c4
lillaknt <lillaknt@gmail.com>
2023-11-03 22:34:04 +0100

    Initial commit

---
+++ Domain/Models/Todo.cs
@@ -0,0 +1,18 @@
+namespace Domain.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public User Owner { get; }
+    public string Title { get; }
+    public bool ISCompleted { get; }
+
+
+
+    public Todo(User owner, string title)
+    {
+        Owner = owner;
+        Title = title;
+    }
+
+}
