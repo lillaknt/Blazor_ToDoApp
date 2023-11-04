88540e6597296d2c32f17b9626a345bcc5eefb51
lillaknt <lillaknt@gmail.com>
2023-11-04 14:22:13 +0100

    Basic setup

--- FileData/DataContainer.cs
+++ FileData/DataContainer.cs
@@ -4,6 +4,7 @@ namespace FileData;
 
 public class DataContainer
 {
+    // we read data from the file and load into these two collections (our "database" tables)
     public ICollection<User> Users { get; set; }
     public ICollection<Todo> Todos { get; set; }
     
--- FileData/FileContext.cs
+++ FileData/FileContext.cs
@@ -1,6 +1,54 @@
+using System.Text.Json;
+using Domain.Models;
+
 namespace FileData;
 
+//responsible for reading and writing the data from and to the file
 public class FileContext
 {
-    
+    private const string filePath = "data.json";
+    private DataContainer? dataContainer; //nullable because we will regularly reset the data, clear it out and reload it
+
+    public ICollection<Todo> Todos
+    {
+        get
+        {
+            LoadData();
+            return dataContainer!.Todos;
+        }
+    }
+
+    public ICollection<User> Users
+    {
+        get
+        {
+            LoadData();
+            return dataContainer!.Users;
+        }
+    }
+
+    private void LoadData()
+    {
+        if (dataContainer != null ) return;
+        if (!File.Exists(filePath))
+        {
+            dataContainer = new()
+            {
+                Todos = new List<Todo>(),
+                Users = new List<User>()
+            };
+            return;
+        }
+
+        string content = File.ReadAllText(filePath);
+        dataContainer = JsonSerializer.Deserialize<DataContainer>(content);
+    }
+
+    public void SaveChanges()
+    {
+        string serialized = JsonSerializer.Serialize(dataContainer);
+        File.WriteAllText(filePath, serialized);
+        dataContainer = null;
+    }
+
 }
